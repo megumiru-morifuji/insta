bec9cff6407e6bfbeef578f627339ea840d903bd
megumiru-morifuji <153786014+megumiru-morifuji@users.noreply.github.com>
2025-06-12 14:12:51 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,14 +5,17 @@ import './App.css'
 import { BrowserRouter,Router, Routes, Route } from 'react-router-dom';
 import {useState,useEffect} from "react";
 
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TopPages />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login email={email} password={password} setEmail={setEmail} setPassword={setPassword} />} />
         <Route path="/Signup" element={<Signup />} />
         {/* 他のルートを追加予定 */}
       </Routes>
